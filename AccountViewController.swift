7a4a59f94c938e49a49d18a929064f1f0700bd6f
Masuda Hinata <hinatamasuda0121@gmail.com>
2021-05-26 20:13:33 +0900

    メール認証のためのコード メール送られてくる

--- AccountViewController.swift
+++ AccountViewController.swift
@@ -1,12 +1,12 @@
-//import UIKit
-import Firebase // Firebaseをインポート
+import UIKit
+import Firebase
 
-class AccountViewController: UIViewController, UITextFieldDelegate {
-    
+class AccountViewController: UIViewController {
+
+    var auth: Auth!
+
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
-    
-    var auth: Auth
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,39 +14,51 @@ class AccountViewController: UIViewController, UITextFieldDelegate {
         emailTextField.delegate = self
         passwordTextField.delegate = self
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
-           super.viewDidAppear(animated)
-           if auth.currentUser != nil {
-               // もし既にユーザーにログインができていれば、タイムラインの画面に遷移する。
-               // このときに、ユーザーの情報を次の画面の変数に値渡ししておく。(直接取得することも可能。)
-               performSegue(withIdentifier: "Timeline", sender: auth.currentUser!)
-           }
-       }
-
-       override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-           let nextViewController = segue.destination as! TimelineViewController
-           let user = sender as! User
-           nextViewController.me = AppUser(data: ["userID": user.uid])
-       }
-
-    // 登録ボタンを押したときに呼ぶメソッド。
-    @IBAction func registerAccount() {
-        let email = emailTextField.text!
-               let password = passwordTextField.text!
-               auth.createUser(withEmail: email, password: password) { (result, error) in
-                   if error == nil, let result = result {
-                       self.performSegue(withIdentifier: "Timeline", sender: result.user)
-                   }
-               }
+        super.viewDidAppear(animated)
+        if auth.currentUser != nil {
+            auth.currentUser?.reload(completion: { error in
+                if error == nil {
+                    if self.auth.currentUser?.isEmailVerified == true {
+                        self.performSegue(withIdentifier: "Timeline", sender: self.auth.currentUser!)
+                    } else if self.auth.currentUser?.isEmailVerified == false {
+                        let alert = UIAlertController(title: "確認用メールを送信しているので確認をお願いします。", message: "まだメール認証が完了していません。", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+            })
+        }
     }
 
-// デリゲートメソッドは可読性のためextensionで分けて記述します。
-    extension AccountViewController: UITextFieldDelegate {
-        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-            textField.resignFirstResponder()
-            return true
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextViewController = segue.destination as! TimelineViewController
+        let user = sender as! User
+        nextViewController.me = AppUser(data: ["userID": user.uid])
+    }
+
+    @IBAction func registerAccount() {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        auth.createUser(withEmail: email, password: password) { (result, error) in
+            if error == nil, let result = result {
+                result.user.sendEmailVerification(completion: { (error) in
+                    if error == nil {
+                        let alert = UIAlertController(title: "仮登録を行いました。", message: "入力したメールアドレス宛に確認メールを送信しました。", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                })
+            }
         }
     }
 }
 
+// デリゲートメソッドは可読性のためextensionで分けて記述します。
+extension AccountViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
