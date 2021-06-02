518a189417b5212f6fda1ae60e7398af3ae435b3
Masuda Hinata <hinatamasuda0121@gmail.com>
2021-06-02 20:13:24 +0900

    目標設定する最初の画面

---
+++ pppppp/GoalViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GoalViewController.swift
+//  pppppp
+//
+//  Created by 増田ひなた on 2021/06/02.
+//
+
+import UIKit
+
+class GoalViewController: UIViewController {
+    
+    @IBOutlet var goalTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
