9d5c77fe3e4d8b75c0383b317436b8a6966b7268
Masakaz Ozaki <masakazozaki@Masakazs-MacBook-Pro.local>
2022-05-03 20:31:00 +0900

    feat: write weightdata to firebase, refactoring

--- pppppp/ViewController.swift
+++ pppppp/ViewController.swift
@@ -3,7 +3,7 @@ import HealthKit
 import Firebase
 import SwiftUI
 
-class ViewController: UIViewController,UITextFieldDelegate {
+class ViewController: UIViewController {
     
     var me: User!
     var auth: Auth!
@@ -11,59 +11,9 @@ class ViewController: UIViewController,UITextFieldDelegate {
     var myHealthStore = HKHealthStore()
     var typeOfBodyMass = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass)!
     var weight: Double!
-//    {
-//        didSet {
-//            DispatchQueue.main.async {
-//                self.titleTextField.text = String(self.weight)
-//            }
-//        }
-//    }
 
-    
-
-    
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var loginLabel: UILabel!
-    
-//    体重を追加する
-    @IBAction func addButton() {
-//        saveWeight(weight: Double(titleTextField.text ?? weight)!)
-    }
-    
-//    設定画面に飛ぶ
-    @IBAction func SettingButton() {
-        
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-        //ログインできてるかどうかの判定
-        if auth.currentUser != nil {
-            auth.currentUser?.reload(completion: { [self] error in
-                if error == nil {
-                    if self.auth.currentUser?.isEmailVerified == true {
-                        return //login ok
-                        loginLabel.text = "ログイン中"
-                    } else {
-                        //メール認証がまだ
-                        if self.auth.currentUser?.isEmailVerified == false {
-                               let alert = UIAlertController(title: "確認用メールを送信しているので確認をお願いします。", message: "まだメール認証が完了していません。", preferredStyle: .alert)
-                               alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                               self.present(alert, animated: true, completion: nil)
-                           }
-                    }
-                }
-            })
-        } else {
-           //user情報なし。ログインにとばす
-               //self.auth.currentUser?.isEmailVerified == true
-               //self.performSegue(withIdentifier: "toCreateAccount", sender: nil)
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                    let secondVC = storyboard.instantiateViewController(identifier: "AccountViewController")
-                    showDetailViewController(secondVC, sender: self)
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -76,16 +26,36 @@ class ViewController: UIViewController,UITextFieldDelegate {
             print(success)
             print(error)
         })
-        
         self.titleTextField?.delegate = self
         read()
-        
-        //label.text = saveData.string(forKey: "key")
-
-        
     }
     
-//    let saveData: UserDefaults = UserDefaults.standard
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //ログインできてるかどうかの判定
+        if auth.currentUser != nil {
+            auth.currentUser?.reload(completion: { [self] error in
+                if error == nil {
+                    if self.auth.currentUser?.isEmailVerified == true {
+                        return //login ok
+                        loginLabel.text = "ログイン中"
+                    } else {
+                        //メール認証がまだ
+                        if self.auth.currentUser?.isEmailVerified == false {
+                            let alert = UIAlertController(title: "確認用メールを送信しているので確認をお願いします。", message: "まだメール認証が完了していません。", preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                            self.present(alert, animated: true, completion: nil)
+                        }
+                    }
+                }
+            })
+        } else {
+            //Loginに飛ばす
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    let secondVC = storyboard.instantiateViewController(identifier: "AccountViewController")
+                    showDetailViewController(secondVC, sender: self)
+        }
+    }
 
     // firebaseにデータの保存.
     func saveWeight(weight: Double) {
@@ -96,32 +66,29 @@ class ViewController: UIViewController,UITextFieldDelegate {
         self.myHealthStore.save(WeightData, withCompletion: {
             (success: Bool, error: Error!) in
             if success {
-                NSLog("成功!")
+                NSLog("HealthKit保存成功!")
             } else {
-                print("失敗")
+                print("HealthKit保存成功!")
             }
         })
         
-    
-
-//        if Auth.auth().currentUser != nil {
-//            let dataStore = Firestore.firestore()
-//            let db = Firestore.firestore()
-//            db.collection("UserData")
-//                        .document("UUID()")
-//                        .collection("weightData") // サブコレクションであるprefecturesがない場合、自動でリストが生成される。
-//                        .document("weightData()")
-//                        .setData([
-//                            "weight": "weight",
-//            ]) { err in
-//                if let err = err {
-//                    print("Error writing document: \(err)")
-//                } else {
-//                    print("Document successfully written!")
-//                }
-//            }
-//
-//        }
+        if let currentUser = Auth.auth().currentUser {
+            let db = Firestore.firestore()
+            db.collection("UserData")
+                .document(currentUser.uid)
+                .collection("weightData")
+                        .document("\(Date())") // サブコレクションであるprefecturesがない場合、自動でリストが生成される。
+                        .setData([
+                            "weight": String(weight),
+                            "date"  : Date(),
+            ]) { err in
+                if let err = err {
+                    print("Error writing document: \(err)")
+                } else {
+                    print("Document successfully written!")
+                }
+            }
+        }
     }
     
     //データを取得
@@ -134,6 +101,14 @@ class ViewController: UIViewController,UITextFieldDelegate {
         myHealthStore.execute(query)
     }
     
+    @IBAction func addButtonPressed() {
+        guard let inputWeightText = titleTextField.text else { return }
+        guard let inputWeight = Double(inputWeightText) else { return }
+        saveWeight(weight: inputWeight)
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
     // キーボードを閉じる
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
             titleTextField.resignFirstResponder()
