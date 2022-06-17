2e71d08e7d6af40137f32ec02e60f876a9cff48e
masudahinata <hinatamasuda0121@gmail.com>
2022-06-17 16:23:35 +0900

    MeとdashBoardに分けた

--- pppppp/ViewController.swift
+++ pppppp/ViewController.swift
@@ -8,32 +8,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
     var me: User!
     var auth: Auth!
     let db = Firestore.firestore()
-    var myHealthStore = HKHealthStore()
-    var typeOfBodyMass = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass)!
-    var weight: Double!
     
-    @IBOutlet var titleTextField: UITextField!
     @IBOutlet var loginLabel: UILabel!
-    @IBAction func addButtonPressed() {
-        guard let inputWeightText = titleTextField.text else { return }
-        guard let inputWeight = Double(inputWeightText) else { return }
-        saveWeight(weight: inputWeight)
-        print("体重を保存しました")
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         auth = Auth.auth()
         
-        let types = Set([typeOfBodyMass])
-        let healthStore = HKHealthStore()
-        healthStore.requestAuthorization(toShare: types, read: types, completion: { success, error in
-            print(success)
-            print(error)
-        })
-        self.titleTextField?.delegate = self
-        read()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -63,56 +45,5 @@ class ViewController: UIViewController, UITextFieldDelegate {
             showDetailViewController(secondVC, sender: self)
         }
     }
-    
-    // firebaseにデータの保存.
-    func saveWeight(weight: Double) {
-        
-        let quantity = HKQuantity(unit: HKUnit.gramUnit(with: .kilo), doubleValue: weight)
-        let WeightData = HKQuantitySample(type: typeOfBodyMass, quantity: quantity, start: Date(), end: Date())
-        
-        self.myHealthStore.save(WeightData, withCompletion: {
-            (success: Bool, error: Error!) in
-            if success {
-                NSLog("HealthKit保存成功!")
-            } else {
-                print("HealthKit保存成功!")
-            }
-        })
-        
-        
-        if let currentUser = Auth.auth().currentUser {
-            let db = Firestore.firestore()
-            db.collection("UserData")
-                .document(currentUser.uid)
-                .collection("weightData")
-                .document("\(Date())") // サブコレクションであるprefecturesがない場合、自動でリストが生成される。
-                .setData([
-                    "weight": String(weight),
-                    "date"  : Date(),
-                ]) { err in
-                    if let err = err {
-                        print("Error writing document: \(err)")
-                    } else {
-                        print("Document successfully written!")
-                    }
-                }
-        }
-    }
-    
-    //データを取得
-    func read() {
-        let query = HKSampleQuery(sampleType: typeOfBodyMass, predicate: nil, limit: 1, sortDescriptors: nil) { (query, results, error) in
-            if results is [HKQuantitySample] {
-                print("データを取得しました")
-            }
-        }
-        myHealthStore.execute(query)
-    }
-    
-    func textFieldShouldReturn(_ titleTextField: UITextField) -> Bool {
-        titleTextField.resignFirstResponder()
-        return true
-    }
-    
 }
 
