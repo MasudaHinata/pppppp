09a4211b17946e7a0883b8c9d14c010df902d118
masudahinata <hinatamasuda0121@gmail.com>
2022-07-05 17:51:42 +0900

    体重とってこれたけど要らない情報がたくさんあるから日付と体重だけ撮りたい

--- pppppp/MeViewController.swift
+++ pppppp/MeViewController.swift
@@ -17,6 +17,7 @@ class MeViewController: UIViewController {
     var weight: Double!
     let userID = Auth.auth().currentUser!.uid
     
+    
     @IBOutlet var weightTextField: UITextField!
     @IBOutlet var mynameLabel: UILabel!
     @IBAction func addButtonPressed() {
@@ -24,8 +25,8 @@ class MeViewController: UIViewController {
         guard let inputWeight = Double(inputWeightText) else { return }
         saveWeight(weight: inputWeight)
     }
-//    @IBOutlet var queryStatusLabel: UITextView!
-
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +40,7 @@ class MeViewController: UIViewController {
         let readTypes = Set([
             HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount )!
         ])
-
+        
         myHealthStore.requestAuthorization(toShare: [], read: readTypes, completion: { success, error in
             print(success)
         })
@@ -53,87 +54,44 @@ class MeViewController: UIViewController {
         
         self.weightTextField?.delegate = self
         getname()
-        //read()
     }
-        //HealthKitに体重を保存.
-        func saveWeight(weight: Double) {
+    //HealthKitに体重を保存.
+    func saveWeight(weight: Double) {
+        
+        let quantity = HKQuantity(unit: HKUnit.gramUnit(with: .kilo), doubleValue: weight)
+        let WeightData = HKQuantitySample(type: typeOfBodyMass, quantity: quantity, start: Date(), end: Date())
+        
+        self.myHealthStore.save(WeightData, withCompletion: {
+            (success: Bool, error: Error!) in
+            if success {
+                NSLog("HealthKit保存成功!")
+            } else {
+                print("HealthKitに保存できませんでした。")
+            }
+        })
+        
+    }
     
-            let quantity = HKQuantity(unit: HKUnit.gramUnit(with: .kilo), doubleValue: weight)
-            let WeightData = HKQuantitySample(type: typeOfBodyMass, quantity: quantity, start: Date(), end: Date())
+    //    身長を取得
+    func readHeight() {
+        
+    }
     
-            self.myHealthStore.save(WeightData, withCompletion: {
-                (success: Bool, error: Error!) in
-                if success {
-                    NSLog("HealthKit保存成功!")
-                } else {
-                    print("HealthKitに保存できませんでした。")
+    //    体重データをHealthkitから取得
+    @IBAction func readweight() {
+        DispatchQueue.main.async { [self] in
+            let query = HKSampleQuery(sampleType: self.typeOfBodyMass, predicate: nil, limit: Int(Float(0.1)), sortDescriptors: nil) { (query, results, error) in
+                if results is [HKQuantitySample] {
+                    if results is [HKQuantitySample] {
+                        // 取得したデータを格納
+                        
+                        print("体重は\(String(describing: results))")
+                    }
                 }
-            })
-    
+            }
+            myHealthStore.execute(query)
         }
-    
-//    身長を取得
-    func readHeight (){
-//            let type = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight)
-            let query = HKSampleQuery(sampleType: type!, predicate: nil, limit: 10, sortDescriptors: nil, resultsHandler: { query, data, error in
-                print(query)
-                print(data) // → Optional([180 m "MyApp" (1) 2016-05-08 13:16:51 +0900 2016-05-08 13:16:51 +0900])
-                print(error)
-            })
-            print(myHealthStore.execute(query))
     }
-
-    //体重データをHealthkitから取得
-
-    
-    
-    //体重データをHealthkitから取得
-//    @IBAction func getBodyMass(_ sender: Any) {
-//            let start = Calendar.current.date(byAdding: .month, value: -48, to: Date())
-//            let end = Date()
-//            let predicate = HKQuery.predicateForSamples(withStart: start, end: end)
-//            let sampleType = HKQuantityType.quantityType(forIdentifier: .bodyMass)!
-//
-//            let query = HKSampleQuery(
-//                sampleType: sampleType,
-//                predicate: predicate,
-//                limit: HKObjectQueryNoLimit,
-//                sortDescriptors: nil) {
-//                (query, results, error) in
-//
-//                let samples = results as! [HKQuantitySample]
-//
-//                var buf = ""
-//                for sample in samples {
-//                    // Process each sample here.
-//                    let s = sample.quantity
-//                    print("\(String(describing: sample))")
-//
-//                    buf.append("\(sample.startDate) \(String(describing: s))\n")
-//                }
-//
-//                DispatchQueue.main.async {
-//                    self.queryStatusLabel.text = "\(buf)"
-//                }
-//            }
-//            self.myHealthStore.execute(query)
-//        }
-
-    //体重データをHealthkitから取得
-    //    func read() {
-    //        DispatchQueue.main.async { [self] in
-    //            let query = HKSampleQuery(sampleType: self.typeOfBodyMass, predicate: nil, limit: Int(Float(0.1)), sortDescriptors: nil) { (query, results, error) in
-    //                if results is [HKQuantitySample] {
-    //                    if results is [HKQuantitySample] {
-    //                        // 取得したデータを格納
-    //                        self.weightLabel.text = "体重は\(String(describing: results))"
-    //                        print("体重は\(String(describing: results))")
-    //                    }
-    //                }
-    //            }
-    //            myHealthStore.execute(query)
-    //        }
-    //    }
     
     //    名前を表示
     func getname() {
