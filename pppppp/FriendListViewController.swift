f7b3426c2b7b50f6a4ee93f610613c5c37e8ed5a
masudahinata <hinatamasuda0121@gmail.com>
2022-08-08 09:13:31 +0900

    画面のデザイん

--- pppppp/FriendListViewController.swift
+++ pppppp/FriendListViewController.swift
@@ -19,7 +19,6 @@ final class FriendListViewController: UIViewController, FirebaseClientDelegate {
         }
     }
     var cancellables = Set<AnyCancellable>()
-    @IBOutlet var mynameLabel: UILabel!
     
     @IBAction func dataputButton() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
