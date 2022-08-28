d807d707e7f2148f43660a72b750732ef08d7a8c
MasudaHinata <hinatamasuda0121@gmail.com>
2022-08-28 19:02:37 +0900

    通知の設定

--- pppppp/NotificationManager.swift
+++ pppppp/NotificationManager.swift
@@ -9,17 +9,23 @@ import Foundation
 import NotificationCenter
 
 class NotificationManager {
-    
     static func setCalendarNotification(title: String, body: String) {
-        UserNotifications.UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound]) { granted, error in
-            if granted {
-                let dateComponents = DateComponents(calendar: Calendar.current, timeZone: TimeZone.current, hour: 19, minute: 00)
-                let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-                let notificationContent = UNMutableNotificationContent()
-                notificationContent.title = title
-                notificationContent.body = body
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: notificationContent, trigger: trigger)
-                UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        let notificationContent = UNMutableNotificationContent()
+        notificationContent.title = title
+        notificationContent.body = body
+        notificationContent.badge = NSNumber(value: 1)
+        notificationContent.sound = .default
+        
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        
+        var datComp = DateComponents()
+        datComp.hour = 18
+        datComp.minute = 52
+        let trigger = UNCalendarNotificationTrigger(dateMatching: datComp, repeats: true)
+        let request = UNNotificationRequest(identifier: "ID", content: notificationContent, trigger: trigger)
+        UNUserNotificationCenter.current().add(request) { (error : Error?) in
+            if let theError = error {
+                print(theError.localizedDescription)
             }
         }
     }
