edaf3ee20c94321e8d781be3a22045bf9328f908
MasudaHinata <hinatamasuda0121@gmail.com>
2022-09-07 00:57:21 +0900

    通知が片方鳴らなくなる

--- pppppp/Manager/NotificationManager.swift
+++ pppppp/Manager/NotificationManager.swift
@@ -11,8 +11,8 @@ class NotificationManager {
         UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
         
         var datComp = DateComponents()
-        datComp.hour = 19
-        datComp.minute = 00
+        datComp.hour = 00
+        datComp.minute = 57
         let trigger = UNCalendarNotificationTrigger(dateMatching: datComp, repeats: true)
         let request = UNNotificationRequest(identifier: "ID", content: notificationContent, trigger: trigger)
         UNUserNotificationCenter.current().add(request) { (error : Error?) in
@@ -21,23 +21,23 @@ class NotificationManager {
             }
         }
     }
-    static func dailyNotification(title: String, body: String) {
-        let notificationContent = UNMutableNotificationContent()
-        notificationContent.title = title
-        notificationContent.body = body
-        notificationContent.badge = NSNumber(value: 1)
-        notificationContent.sound = .default
-        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
-        
-        var datComp = DateComponents()
-        datComp.hour = 7
-        datComp.minute = 00
-        let trigger = UNCalendarNotificationTrigger(dateMatching: datComp, repeats: true)
-        let request = UNNotificationRequest(identifier: "dailyNotification", content: notificationContent, trigger: trigger)
-        UNUserNotificationCenter.current().add(request) { (error : Error?) in
-            if let theError = error {
-                print(theError.localizedDescription)
-            }
-        }
-    }
+//    static func dailyNotification(title: String, body: String) {
+//        let notificationContent = UNMutableNotificationContent()
+//        notificationContent.title = title
+//        notificationContent.body = body
+//        notificationContent.badge = NSNumber(value: 1)
+//        notificationContent.sound = .default
+//        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+//
+//        var datComp = DateComponents()
+//        datComp.hour = 00
+//        datComp.minute = 56
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: datComp, repeats: true)
+//        let request = UNNotificationRequest(identifier: "dailyNotification", content: notificationContent, trigger: trigger)
+//        UNUserNotificationCenter.current().add(request) { (error : Error?) in
+//            if let theError = error {
+//                print(theError.localizedDescription)
+//            }
+//        }
+//    }
 }
