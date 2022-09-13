aa6f21d604b2bcdcdae66ddb155b3b4c5edc7cc3
MasudaHinata <hinatamasuda0121@gmail.com>
2022-09-13 21:32:22 +0900

    applelogin

--- pppppp/View/AuthView/LoginViewController.swift
+++ pppppp/View/AuthView/LoginViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Combine
-//import AuthenticationServices
+import AuthenticationServices
+import CryptoKit
+import Firebase
 
 @MainActor
 class LoginViewController: UIViewController, FirebaseClientAuthDelegate {
@@ -8,7 +10,6 @@ class LoginViewController: UIViewController, FirebaseClientAuthDelegate {
     var cancellables = Set<AnyCancellable>()
     var loginEmailAdress: String?
     
-//    @IBOutlet var buttonView: UIView!
     @IBOutlet var loginLabel: UILabel!
     @IBOutlet var loginButtonLayout: UIButton! {
         didSet {
@@ -74,8 +75,9 @@ class LoginViewController: UIViewController, FirebaseClientAuthDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        buttonView.addSubview(signinButton)
-//        signinButton.fitConstraintsContentView(view: buttonView)
+        
+        buttonView.addSubview(signinButton)
+        signinButton.fitConstraintsContentView(view: buttonView)
         
         FirebaseClient.shared.loginDelegate = self
         let tapGR: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
@@ -96,30 +98,95 @@ class LoginViewController: UIViewController, FirebaseClientAuthDelegate {
         present(alert, animated: true)
     }
     
-//    private lazy var signinButton: ASAuthorizationAppleIDButton = {
-//        let button = ASAuthorizationAppleIDButton(
-//            type: .default,
-//            style: .white
-//        )
-//        button.addTarget(
-//            self,
-//            action: #selector(handleSignin),
-//            for: .touchUpInside
-//        )
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        button.bounds = CGRect(x: 0, y: 0, width: 240, height: 48)
-//        return button
-//    }()
-//
-//    @objc private func handleSignin() {
-//        let appleIDProvider = ASAuthorizationAppleIDProvider()
-//        let request = appleIDProvider.createRequest()
-//        request.requestedScopes = [.email]
-//        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-//        authorizationController.delegate = self
-//        authorizationController.presentationContextProvider = self
-//        authorizationController.performRequests()
-//    }
+    @IBOutlet var buttonView: UIView!
+    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    private func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+      var result = ""
+      var remainingLength = length
+
+      while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+          var random: UInt8 = 0
+          let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+          if errorCode != errSecSuccess {
+            fatalError(
+              "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+            )
+          }
+          return random
+        }
+
+        randoms.forEach { random in
+          if remainingLength == 0 {
+            return
+          }
+
+          if random < charset.count {
+            result.append(charset[Int(random)])
+            remainingLength -= 1
+          }
+        }
+      }
+
+      return result
+    }
+    @available(iOS 13, *)
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
+    }
+
+    // Unhashed nonce.
+    fileprivate var currentNonce: String?
+
+    @available(iOS 13, *)
+    func startSignInWithAppleFlow() {
+      let nonce = randomNonceString()
+      currentNonce = nonce
+      let appleIDProvider = ASAuthorizationAppleIDProvider()
+      let request = appleIDProvider.createRequest()
+      request.requestedScopes = [.fullName, .email]
+      request.nonce = sha256(nonce)
+
+      let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+      authorizationController.delegate = self
+      authorizationController.presentationContextProvider = self
+      authorizationController.performRequests()
+    }
+        
+    private lazy var signinButton: ASAuthorizationAppleIDButton = {
+        let button = ASAuthorizationAppleIDButton(
+            type: .default,
+            style: .white
+        )
+        button.addTarget(
+            self,
+            action: #selector(handleSignin),
+            for: .touchUpInside
+        )
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.bounds = CGRect(x: 0, y: 0, width: 240, height: 48)
+        startSignInWithAppleFlow()
+        return button
+    }()
+
+    @objc private func handleSignin() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.email]
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
     
     //MARK: - Setting Delegate
     func loginScene() {
@@ -138,47 +205,64 @@ extension LoginViewController: UITextFieldDelegate {
     }
 }
 
-//extension LoginViewController: ASAuthorizationControllerDelegate {
-//    func authorizationController(
-//        controller: ASAuthorizationController,
-//        didCompleteWithAuthorization authorization: ASAuthorization
-//    ) {
-//        // AppleIDログイン完了時にはしる処理。サーバにAuth情報を保存したりする。
-//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//        let secondVC = storyboard.instantiateViewController(identifier: "TabBarViewController")
-//        self.showDetailViewController(secondVC, sender: self)
-//    }
-//
-//    func authorizationController(
-//        controller: ASAuthorizationController,
-//        didCompleteWithError error: Error
-//    ) {
-//        let alert = UIAlertController(
-//            title: "Error",
-//            message: error.localizedDescription,
-//            preferredStyle: .alert
-//        )
-//        alert.addAction(UIAlertAction(
-//            title: "OK",
-//            style: .default)
-//        )
-//    }
-//}
-//
-//extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
-//    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-//        return self.view.window!
-//    }
-//}
-//extension UIView {
-//    func fitConstraintsContentView(view: UIView? = nil) {
-//        if let contentView = view == nil ? subviews.first : view {
-//            NSLayoutConstraint.activate([
-//                contentView.topAnchor.constraint(equalTo: self.topAnchor),
-//                contentView.leftAnchor.constraint(equalTo: self.leftAnchor),
-//                contentView.rightAnchor.constraint(equalTo: self.rightAnchor),
-//                contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
-//            ])
-//        }
-//    }
-//}
+@available(iOS 13.0, *)
+extension LoginViewController: ASAuthorizationControllerDelegate {
+
+  func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+    if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+      guard let nonce = currentNonce else {
+        fatalError("Invalid state: A login callback was received, but no login request was sent.")
+      }
+      guard let appleIDToken = appleIDCredential.identityToken else {
+        print("Unable to fetch identity token")
+        return
+      }
+      guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+        print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+        return
+      }
+      // Initialize a Firebase credential.
+      let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                idToken: idTokenString,
+                                                rawNonce: nonce)
+      // Sign in with Firebase.
+      Auth.auth().signIn(with: credential) { (authResult, error) in
+          if (error != nil) {
+          // Error. If error.code == .MissingOrInvalidNonce, make sure
+          // you're sending the SHA256-hashed nonce as a hex string with
+          // your request to Apple.
+            print(error!.localizedDescription)
+          return
+        }
+        // User is signed in to Firebase with Apple.
+        // ...
+          let storyboard = UIStoryboard(name: "Main", bundle: nil)
+          let secondVC = storyboard.instantiateViewController(identifier: "TabBarViewController")
+          self.showDetailViewController(secondVC, sender: self)
+      }
+    }
+  }
+
+  func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+    // Handle error.
+    print("Sign in with Apple errored: \(error)")
+  }
+
+}
+extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+}
+extension UIView {
+    func fitConstraintsContentView(view: UIView? = nil) {
+        if let contentView = view == nil ? subviews.first : view {
+            NSLayoutConstraint.activate([
+                contentView.topAnchor.constraint(equalTo: self.topAnchor),
+                contentView.leftAnchor.constraint(equalTo: self.leftAnchor),
+                contentView.rightAnchor.constraint(equalTo: self.rightAnchor),
+                contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+            ])
+        }
+    }
+}
