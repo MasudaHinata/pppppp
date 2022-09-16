ccc2260b9bd88f75fb0da47f55b27f1c802244a2
MasudaHinata <hinatamasuda0121@gmail.com>
2022-09-16 23:49:01 +0900

    extensionを分けてる

--- pppppp/View/ProfileData/UserDataViewController.swift
+++ pppppp/View/ProfileData/UserDataViewController.swift
@@ -92,71 +92,8 @@ class UserDataViewController: UIViewController {
 }
 
 //MARK: - extension
-extension UserDataViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 112
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SummaryCollectionViewCell", for: indexPath)  as! SummaryCollectionViewCell
-        
-        let weekday = Int(indexPath.row / 16) // 1行目は0
-        let todayWeekday = Calendar.current.component(.weekday, from: Date()) - 1 // 1から始まるので揃えるために1引く
-        let weekdayDelta = todayWeekday - weekday  //いくつ前の曜日か
-        let weekDelta = 15 - indexPath.row % 16 //何週前か
-        
-        var dayForCell = Date()
-        dayForCell = Calendar.current.date(byAdding: .weekOfYear, value: -weekDelta, to: dayForCell)!
-        dayForCell = Calendar.current.date(byAdding: .weekday, value: -weekdayDelta, to: dayForCell)!
-        let activitiesForCell = pointDataList.filter { $0.date.getZeroTime() == dayForCell.getZeroTime() }.compactMap { $0.point }
-        if dayForCell.getZeroTime() > Date().getZeroTime() {
-            cell.backgroundColor = UIColor(hex: "FFFFFF", alpha: 0)
-            return cell
-        }
-        let totalPointsForCell = activitiesForCell.reduce(0, +) // 合計
-        switch totalPointsForCell {
-        case 0 :
-            cell.backgroundColor = UIColor(hex: "FFFFFF", alpha: 0.46)
-        case 1...30:
-            cell.backgroundColor = UIColor(hex: "45E1FF", alpha: 0.46)
-        case 30...70:
-            cell.backgroundColor = UIColor(hex: "3D83BC", alpha: 0.46)
-        case 70...100:
-            cell.backgroundColor = UIColor(hex: "008DDC", alpha: 0.46)
-        default:
-            cell.backgroundColor = UIColor(hex: "1D5CAC", alpha: 0.46)
-        }
-        return cell
-    }
-}
 extension Date {
     func getZeroTime() -> Date {
         Calendar.current.startOfDay(for: self)
-    }
-}
-
-extension UserDataViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        pointDataList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RecentActivitysTableViewCell", for: indexPath) as! RecentActivitysTableViewCell
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MM/dd"
-        let dataStr = dateFormatter.string(from: pointDataList[indexPath.row].date)
-        
-        cell.pointLabel.text = "+\(pointDataList[indexPath.row].point ?? 0)pt"
-        cell.dateLabel.text = dataStr
-        cell.activityLabel.text = pointDataList[indexPath.row].activity ?? ""
-        return cell
-    }
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        //ヘッダーの肥大化を回避
-        return "  "
-    }
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 4
     }
 }
