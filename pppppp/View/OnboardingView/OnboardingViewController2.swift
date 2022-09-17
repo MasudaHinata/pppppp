703198cd2a7378d880cbe436d92aa8ffd8bfdc65
MasudaHinata <hinatamasuda0121@gmail.com>
2022-09-17 23:35:57 +0900

    初期画面

--- pppppp/View/OnboardingView/OnboardingViewController2.swift
+++ pppppp/View/OnboardingView/OnboardingViewController2.swift
@@ -29,6 +29,5 @@ class OnboardingViewController2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 }
