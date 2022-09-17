23b679920191d50cd885cb131216992e9a34ad45
MasudaHinata <hinatamasuda0121@gmail.com>
2022-09-18 00:25:23 +0900

    design

--- pppppp/View/AddFriendsView/AddFriendViewController.swift
+++ pppppp/View/AddFriendsView/AddFriendViewController.swift
@@ -16,6 +16,12 @@ class AddFriendViewController: UIViewController {
         let secondVC = storyboard.instantiateInitialViewController()
         self.showDetailViewController(secondVC!, sender: self)
     }
+    
+    @IBAction func sceneSetting() {
+        let storyboard = UIStoryboard(name: "SettingView", bundle: nil)
+        let secondVC = storyboard.instantiateInitialViewController()
+        self.showDetailViewController(secondVC!, sender: self)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
