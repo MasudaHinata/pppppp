31736b9eb1245fa56edf0c78fab624bc33b7d094
MasudaHinata <hinatamasuda0121@gmail.com>
2022-09-21 17:43:01 +0900

    FriendList触ったらUserDataView出る

--- pppppp/View/Friend&MeDataView/ProfileViewController+CollectionViewDelegate.swift
+++ pppppp/View/Friend&MeDataView/ProfileViewController+CollectionViewDelegate.swift
@@ -51,40 +51,16 @@ extension ProfileViewController: UICollectionViewDataSource, UICollectionViewDel
             fatalError("collectionView Tag Invalid")
         }
     }
-    //友達を削除する
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if collectionView.tag == 1 {
             //MARK: - activityGrass
         } else if collectionView.tag == 0 {
-            //MARK: - friendList
-            guard let deleteFriendId = friendDataList[indexPath.row].id else { return }
-            let alert = UIAlertController(title: "注意", message: "友達を削除しますか？", preferredStyle: .alert)
-            let delete = UIAlertAction(title: "削除", style: .destructive, handler: { [self] (action) -> Void in
-                let task = Task { [weak self] in
-                    guard let self = self else { return }
-                    do {
-                        try await FirebaseClient.shared.deleteFriendQuery(deleteFriendId: deleteFriendId)
-                        friendDataList = try await FirebaseClient.shared.getProfileData(includeMe: false)
-                        self.friendcollectionView.reloadData()
-                    }
-                    catch {
-                        print("ProfileViewController collectionview error:",error.localizedDescription)
-                        if error.localizedDescription == "Network error (such as timeout, interrupted connection or unreachable host) has occurred." {
-                            ShowAlertHelper.okAlert(vc: self, title: "エラー", message: "インターネット接続を確認してください", handler: { (_) in
-                                self.viewDidLoad()
-                            })
-                        } else {
-                            ShowAlertHelper.okAlert(vc: self, title: "エラー", message: "\(error.localizedDescription)", handler: { (_) in })
-                        }
-                    }
-                }
-                cancellables.insert(.init { task.cancel() })
-            })
-            let cancel = UIAlertAction(title: "キャンセル", style: .cancel, handler: { (action) -> Void in
-            })
-            alert.addAction(delete)
-            alert.addAction(cancel)
-            self.present(alert, animated: true, completion: nil)
+            //MARK: - friendList UserDataViewに遷移
+            let storyboard = UIStoryboard(name: "UserDataView", bundle: nil)
+            let secondVC = storyboard.instantiateInitialViewController() as! UserDataViewController
+            secondVC.userDataItem = friendDataList[indexPath.row]
+            self.showDetailViewController(secondVC, sender: self)
         } else {
             fatalError("collectionView Tag Invalid")
         }
