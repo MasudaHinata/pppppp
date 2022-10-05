d3c9fa13d9bb25e115900b7c10bb73e6b63b6de7
MasudaHinata <hinatamasuda0121@gmail.com>
2022-10-05 21:49:00 +0900

    体重と目標体重を入力

--- pppppp/View/OnboardingView/SetGoalWeightView/SetGoalWeightViewController.swift
+++ pppppp/View/OnboardingView/SetGoalWeightView/SetGoalWeightViewController.swift
@@ -1,6 +1,9 @@
 import UIKit
+import Combine
 
 class SetGoalWeightViewController: UIViewController {
+    
+    var cancellables = Set<AnyCancellable>()
     
     @IBOutlet var weightTextField: UITextField! {
         didSet {
@@ -27,18 +30,50 @@ class SetGoalWeightViewController: UIViewController {
             saveWeightGoalButtonLayout.configuration = configuration
         }
     }
-
-    @IBAction func saveWeightGoalButton(_ sender: Any) {
-        
     
+    @IBAction func saveWeightGoalButton(_ sender: Any) {
+        guard let inputWeightText = weightTextField.text else { return }
+        guard let inputWeight = Double(inputWeightText) else {
+            ShowAlertHelper.okAlert(vc: self, title: "エラー", message: "体重を入力してください", handler: { _ in })
+            return
+        }
+        
+        guard let inputWeightGoalText = weightGoalTextField.text else { return }
+        guard let inputWeightGoal = Double(inputWeightGoalText) else {
+            ShowAlertHelper.okAlert(vc: self, title: "エラー", message: "目標体重を入力してください", handler: { _ in })
+            return
+            
+        }
+        
+        UserDefaults.standard.set(inputWeightGoal, forKey: "weightGoal")
+        
+        let task = Task { [weak self] in
+            guard let self = self else { return }
+            do {
+                try await HealthKit_ScoreringManager.shared.writeWeight(weight: inputWeight)
+                ShowAlertHelper.okAlert(vc: self, title: "完了", message: "体重と目標体重を記録しました", handler: { _ in })
+            }
+            catch {
+                print("SetGoalWeightViewDid error:", error.localizedDescription)
+                ShowAlertHelper.okAlert(vc: self, title: "エラー", message: "\(error.localizedDescription)", handler: { _ in })
+            }
+        }
+        self.cancellables.insert(.init { task.cancel() })
+        
         let secondVC = StoryboardScene.Main.initialScene.instantiate()
         self.showDetailViewController(secondVC, sender: self)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         UserDefaults.standard.set(true, forKey: "initialScreen")
+        
+        let tapGR: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGR.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tapGR)
     }
     
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
 }
