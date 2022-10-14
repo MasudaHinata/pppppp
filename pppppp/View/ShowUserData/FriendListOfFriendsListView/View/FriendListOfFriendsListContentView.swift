9ed7ae2e94c7632ba0ec2a69bdda859a80d063f7
MasudaHinata <hinatamasuda0121@gmail.com>
2022-10-14 17:06:23 +0900

    design

--- pppppp/View/ShowUserData/FriendListOfFriendsListView/View/FriendListOfFriendsListContentView.swift
+++ pppppp/View/ShowUserData/FriendListOfFriendsListView/View/FriendListOfFriendsListContentView.swift
@@ -12,38 +12,30 @@ struct FriendListOfFriendsListContentView: View {
         let width = bounds.width
 
         NavigationView {
-            ScrollView {
-                VStack {
-                    Text("").fontWeight(.semibold)
-                        .padding(.horizontal, 16.0)
-                        .frame(maxWidth: width, maxHeight: 4,alignment: .leading)
+            Form {
+                ForEach(viewModel.userData) { item in
+                    HStack {
+                        KFImage(URL(string: item.iconImageURL))
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 64, height: 64)
+                            .cornerRadius(32)
 
-                    ForEach(viewModel.userData) { item in
-                        HStack {
-                            KFImage(URL(string: item.iconImageURL))
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 64, height: 64)
-                                .cornerRadius(32)
-
-                            Text(item.name)
-                                .fontWeight(.semibold)
-                                .frame(width: 104, height: 32, alignment: .leading)
+                        Text(item.name)
+                            .fontWeight(.semibold)
+                            .frame(width: 104, height: 32, alignment: .leading)
 
 
-                            Button {
-                                viewModel.sceneAddFriendView()
-                            } label: {
-                                Text("追加")
-                                    .frame(width: 56, height: 24)
-                                    .fontWeight(.regular)
-                                    .background(Color(asset: Asset.Colors.white00))
-                                    .foregroundColor(Color(asset: Asset.Colors.mainColor))
-                            }
+                        Button {
+                            viewModel.sceneAddFriendView()
+                        } label: {
+
                         }
                     }
                 }
+                .listRowBackground(Color.clear)
             }
+            .scrollContentBackground(.hidden)
             .background(Color(asset: Asset.Colors.mainColor))
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .navigationBarTitle(Text("Friends"))
